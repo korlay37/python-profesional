5c0b815235f2d14e38952d602201f9fecbab7719
korlay37 <ercorlay7_@msn.com>
2025-01-24 14:08:19 -0500

    Primer clase

---
+++ 01_python_a_fondo/01_funciones_integradas.py
@@ -0,0 +1,45 @@
+
+# print()
+# print("Mi", "nombre", "es", "Eduardo", sep="\n")
+# import time
+
+# for i in range(3):
+#     print(f"Cargando {i}...", end="", flush=True)
+#     time.sleep
+
+# isinstance()
+# x: int = 10
+# print(isinstance(x, int))
+# print(isinstance(x, str))
+
+# enumerate
+# frutas: list[str] = ["manzana", "pera", "naranja"]
+# for indice, fruta in enumerate(frutas):
+#     print(f"{indice}: {fruta}")
+
+# round(numero, ndigits)
+# numero: float = 12.1234
+# print(round(numero, 2))
+# print(round(numero))
+
+# range(stop) / range(start, stop, step=1)
+# for i in range(1, 6):
+#     print(i)
+
+# slice(stop) / slice(start, stop, step)
+# numeros: list[int] = [0, 1, 2, 3, 4]
+# print(numeros[slice(1,3)])
+
+# globals / locals
+# from pprint import pp
+# x: int = 42
+# def ejemplo() -> None:
+#     y: int = 10
+#     print("Locales: ", locals())
+
+# ejemplo()
+# # print("Globales: ", globals())
+# print("Globales: ")
+# pp(globals())
+
+
