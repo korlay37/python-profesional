6376885fcca92ecbbcde36bd60cfbe676820622b
korlay37 <ercorlay7_@msn.com>
2025-01-27 10:33:37 -0500

    added lesson

---
+++ 01_python_a_fondo/04_modulo_random.py
@@ -0,0 +1,33 @@
+import random
+
+# Generar numeros random
+# numero: float = random.random()
+# print(f"{numero = }")
+
+# numero_entero: int = random.randint(1, 10)
+# print(f"{numero_entero = }")
+
+# numero_flotante: float = random.uniform(5.5, 8.9)
+# print(f"{numero_flotante = }")
+
+# Elecciones al azar
+# colores: list[str] = ["rojo", "verde", "amarillo", "azul"]
+# color_elegido: str = random.choice(colores)
+# print(f"{color_elegido = }")
+
+# numeros: list[int] = [1,2,3,4,5,6,7,8,9,10]
+# seleccionados: list[int] = random.sample(numeros, 3)
+# print(f"{seleccionados = }")
+
+# Randomizar lista
+# numeros: list[int] = [1,2,3,4,5,6,7,8,9,10]
+# random.shuffle(numeros)
+# print(f"{numeros = }")
+
+# random.seed(22)
+# print(random.random())
+# print(random.random())
+# print("===================")
+# random.seed(22)
+# print(random.random())
+# print(random.random())
