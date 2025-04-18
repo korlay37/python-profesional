ae1ebfe0fac03815f2c567a8b96b42e929c94982
Eduardo Rios <ercorlay7_@msn.com>
2025-04-18 16:27:34 -0400

    Added lesson

--- 05_bases_de_datos/02_sqlalchemy.py
+++ 05_bases_de_datos/02_sqlalchemy.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy.orm import declarative_base, Session, Mapped, mapped_column
 from typing import Optional
 
 # Crear base declarativa
@@ -7,15 +7,15 @@ Base = declarative_base()
 
 # Crear motor
 # "dialect+driver://username:password@host:port/database"
-engine = create_engine("sqlite:///usuarios_orm.db", echo=True)
+engine = create_engine("sqlite:///usuarios_orm.db")
 # engine = create_engine("sqlite:///:memory:")
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Column[int] = Column(Integer, primary_key=True, autoincrement=True)
-    name: Column[str] = Column(String, nullable=False)
-    age: Column[int] = Column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    age: Mapped[int] = mapped_column(Integer, nullable=False)
 
     def __repr__(self) -> str:
         return f"User(id={self.id}, name={self.name}, age={self.age})"
@@ -23,14 +23,57 @@ class User(Base):
 # Crear base de datos
 Base.metadata.create_all(engine)
 
-# Insertar y consultar
-with Session(engine) as session:
-    user1: User = User(name="Carlos", age=28)
-    user2: User = User(name="Elena", age=34)
+# Create
+def agregar_usuario(nombre: str, edad: int) -> None:
+    with Session(engine) as session:
+        user: User = User(name=nombre, age=edad)
+        session.add(user)
+        session.commit()
+        print(f"Usuario creadO: {user}")
 
-    session.add_all([user1, user2])
-    session.commit()
+# Read
+def mostrar_usuarios() -> None:
+    with Session(engine) as session:
+        users: list[User] = session.query(User).all()
+        for user in users:
+            print(user)
 
-    usuarios: list[User] = session.query(User).all()
-    for usuario in usuarios:
-        print(usuario)
+def buscar_por_nombre(nombre: str) ->  Optional[User]:
+    with Session(engine) as session:
+        user: Optional[User] = session.query(User).filter(User.name == nombre).first()
+        return user
+
+# Update
+def actualizar_edad(nombre: str, nueva_edad: int) -> None:
+    with Session(engine) as session:
+        user: Optional[User] = session.query(User).filter(User.name == nombre).first()
+        if user:
+            user.age = nueva_edad
+            session.commit()
+            print(f"Edad actualizada {user}")
+        else:
+            print("No se encontro usuario")
+
+# Delete
+def eliminar_usuario(nombre: str) -> None:
+    with Session(engine) as session:
+        user: User = session.query(User).filter(User.name == nombre).first()
+        if user:
+            session.delete(user)
+            session.commit()
+            print(f"Usuario borrado: {user}")
+        else:
+            print("Usuario no encontrado")
+
+agregar_usuario("Paco", 20)
+agregar_usuario("Josefina", 40)
+
+mostrar_usuarios()
+
+actualizar_edad("Josefina", 26)
+user = buscar_por_nombre("Paco")
+print(f"Encontre usuario: {user}")
+
+eliminar_usuario("Paco")
+mostrar_usuarios()
+
