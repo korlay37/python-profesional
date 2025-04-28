07385a9feedc91676345fed78cb1e13111f8fb13
Eduardo Rios <ercorlay7_@msn.com>
2025-04-27 20:45:53 -0400

    Latest

--- 06_pytest/testing/test_sumar.py
+++ 06_pytest/testing/test_sumar.py
@@ -1,5 +1,8 @@
+import pytest
 from modulo_pytest.sumar import sumar
 
+# @pytest.mark.skip(reason="Funcionalidad no implementada")
+@pytest.mark.xfail(reason="Bug conocido en progreso")
 def test_sumar() -> None:
     resultado: int = sumar(2, 5)
-    assert resultado == 7
+    assert resultado == 10
